1e34b267ee98c6b1fc0bfcd013adb5f5c1cbedd1
Gulshan Kumar <inbox.gulshan@outlook.com>
2021-11-25 07:35:20 +0530

    Revert "Fixed a bug in max path sum function where leaf nodes are negative."
    This reverts commit f8601c055f646c47580779a545e9df899f8d4bfd.

--- BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree.cs
@@ -394,12 +394,6 @@ namespace BinaryTree
 
             maxVal = Math.Max(maxVal, (root.value + leftSum + rightSum));
 
-            if(leftSum < 0)
-                leftSum = 0;
-
-            if(rightSum < 0)
-                rightSum = 0;
-
             return root.value + Math.Max(leftSum, rightSum);
         }
         #endregion
